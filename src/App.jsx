673f4e46890b2dc053a0846b8954dd1c9beb171c
akram <akramhany65@gmail.com>
2023-11-26 21:20:34 +0200

    Using the Journey component to display all data

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from "./components/navbar/Navbar.jsx"
 import Journey from './components/journey/Journey.jsx'
+import data from './data.js'
 
 function App() {
+
+  const journies = data.map(journey => 
+    <>
+      <Journey 
+        {...journey}
+      />
+      {journey != data[data.length - 1] ? <hr></hr> : <></>}
+    </>
+    );
+
   return (
     <>
       <Navbar />
       <div className="journies">
-        <Journey 
-          img="./journey1.jpg"
-          country="JAPAN"
-          place="Mount Fuji"
-          startDate="12 Jan, 2021"
-          endDate="24 Jan, 2021"
-          description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        />
+        {journies}
       </div>
-      <hr></hr>
     </>
   )
 }
