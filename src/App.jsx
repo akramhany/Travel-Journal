2ab87e404186352940f3fba01108ca7bcc7b4229
akram <akramhany65@gmail.com>
2023-11-26 20:54:44 +0200

    Make a new journey and style it

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from "./components/navbar/Navbar.jsx"
+import Journey from './components/journey/Journey.jsx'
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="journies">
-        
+        <Journey 
+          img="./journey1.jpg"
+          country="JAPAN"
+          place="Mount Fuji"
+          startDate="12 Jan, 2021"
+          endDate="24 Jan, 2021"
+          description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
       </div>
+      <hr></hr>
     </>
   )
 }
--- src/components/journey/Journey.jsx
+++ src/components/journey/Journey.jsx
@@ -1,9 +1,30 @@
 import React from "react"
+import gps from "./gps.png"
 
-export default function Journey() {
+export default function Journey(props) {
     return (
         <div className="journey">
-            
+            <img src={props.img} alt="" className="journey--img"/>
+            <div className="journey--details">
+                <div  className="country-details">
+                    <img src={gps} alt="" className="gps" />
+                    <h1 className="country-name">
+                        {props.country}
+                    </h1>
+                    <a href="" className="country-address">
+                        View on Google Maps
+                    </a>
+                </div>
+                <h1 className="journey-place">
+                    {props.place}
+                </h1>
+                <h2 className="journey-date">
+                    {`${props.startDate} - ${props.endDate}`}
+                </h2>
+                <p  className="journey-description">
+                    {props.description}
+                </p>
+            </div>
         </div>
     )
 }
